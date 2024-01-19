b912da7e509bc04e39d767c972ac346b8871e525
2001kaushik <140885041+2001kaushik@users.noreply.github.com>
2024-01-19 20:37:41 +0000

    Date

---
+++ .devcontainer/Basics/Date.js
@@ -0,0 +1,12 @@
+let date=new Date()
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toJSON);
+console.log(date.toLocaleDateString());
+
+let mydate= new Date(2024,6,26);
+console.log(mydate.toLocaleDateString());
+
+let mydate2= new Date(2024,6,26,5,6);
+console.log(mydate.toLocaleString());
